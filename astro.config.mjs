adc2df9b8255e2e92f963d5575e90db7444b925b
sonnystark <sonny.stark@posteo.net>
2025-12-10 17:23:54 +0100

    hotfix/added noopener referrerpolicy to external links

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,17 @@
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import { defineConfig } from 'astro/config'
+import rehypeExternalLinks from 'rehype-external-links'
 
 import icon from 'astro-icon'
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://sonnystark.dev',
-  integrations: [mdx(), sitemap(), icon()],
+  integrations: [
+    mdx({
+      rehypePlugins: [[rehypeExternalLinks, { target: '_blank', rel: ['noopener'] }]],
+    }),
+    sitemap(),
+    icon(),
+  ],
 })
